06c06fbf56064c0f51ce9bd2f7fd61f41af4949a
Marcin J <jerzakm@gmail.com>
2019-10-11 12:01:16 +0200

    rollup + svelte

---
+++ src/routes.js
@@ -0,0 +1,58 @@
+import Home from './routes/Home.svelte'
+import Name from './routes/Name.svelte'
+import Wild from './routes/Wild.svelte'
+import Regex from './routes/Regex.svelte'
+import NotFound from './routes/NotFound.svelte'
+
+// This demonstrates how to pass routes as a POJO (Plain Old JavaScript Object) or a JS Map
+// In this code sample we're using both (controlling at runtime what's enabled, by checking for the 'routemap=1' querystring parameter) just because we are using this code sample for tests too
+// In your code, you'll likely want to choose one of the two options only
+let routes
+const urlParams = new URLSearchParams(window.location.search)
+if (!urlParams.has('routemap')) {
+    // The simples way to define routes is to use a dictionary.
+    // This is a key->value pair in which the key is a string of the path.
+    // The path is passed to regexparam that does some transformations allowing the use of params and wildcards
+    routes = {
+        // Exact path
+        '/': Home,
+    
+        // Allow children to also signal link activation
+        '/brand': Home,
+        
+        // Using named parameters, with last being optional
+        '/hello/:first/:last?': Name,
+    
+        // Wildcard parameter
+        '/wild': Wild,
+        '/wild/*': Wild,
+    
+        // Catch-all, must be last
+        '*': NotFound,
+    }
+}
+else {
+    routes = new Map()
+
+    // Exact path
+    routes.set('/', Home)
+
+    // Allow children to also signal link activation
+    routes.set('/brand', Home)
+
+    // Using named parameters, with last being optional
+    routes.set('/hello/:first/:last?', Name)
+
+    // Wildcard parameter
+    routes.set('/wild', Wild)
+    routes.set('/wild/*', Wild)
+
+    // Regular expressions
+    routes.set(/^\/regex\/(.*)?/i, Regex)
+    routes.set(/^\/(pattern|match)(\/[a-z0-9]+)?/i, Regex)
+
+    // Catch-all, must be last
+    routes.set('*', NotFound)
+}
+
+export default routes
--- src/scss/style.js
+++
@@ -1,1 +0,0 @@
-import style from '../scss/main.scss'
