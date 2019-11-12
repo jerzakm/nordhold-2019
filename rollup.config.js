75a52de928f6c1c3d09726474ee9a44941b7328d
Marcin J <jerzakm@gmail.com>
2019-11-12 14:23:42 +0100

    typescript + smui setup

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,26 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
+import postcss from 'rollup-plugin-postcss'
+import scss from 'rollup-plugin-scss'
+import html from 'rollup-plugin-bundle-html'
+import autoPreprocess from 'svelte-preprocess'
+
+import {
+    terser
+} from "rollup-plugin-terser"
+import typescript from "rollup-plugin-typescript2"
+
+const production = false
+
+import {
+    preprocess,
+    createEnv,
+    readConfigFile
+} from "@pyoner/svelte-ts-preprocess";
 
 export default {
-    input: 'src/main.js',
+    input: 'src/main.ts',
     output: {
         sourcemap: true,
         format: 'iife',
@@ -16,37 +33,53 @@ export default {
         exclude: ['node_modules/**']
     },
     plugins: [
+        scss(),
         svelte({
             // enable run-time checks when not in production
-            dev: true,
+            dev: !production,
             // we'll extract any component CSS out into
             // a separate file  better for performance
-            css: css => {
-                css.write('dist/bundle.css')
-            }
+            // css: css => {
+            //     css.write('dist/bundle.css')
+            // },
+            preprocess: autoPreprocess()
         }),
-
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration 
-        // consult the documentation for details:
-        // https://github.com/rollup/rollup-plugin-commonjs
+        html({
+            template: 'src/index.html',
+            dest: "dist",
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        postcss({
+            extract: true,
+            minimize: false,
+            use: [
+                ['sass', {
+                    includePaths: [
+                        './src/styles/theme',
+                        './node_modules'
+                    ]
+                }]
+            ]
+        }),
         resolve(),
         commonjs(),
+        typescript(),
         serve({
             // Launch in browser (default: false)
-            open: true,           
-           
+            open: true,
+
             // Folder to serve files from
             contentBase: '',
-           
+
             // Multiple folders to serve from
-            contentBase: ['dist'],     
-           
+            contentBase: ['dist'],
+
             // Options used in setting up server
             host: 'localhost',
-            port: 3000,     
-          }),
+            port: 3000,
+        }),
+
         livereload()
     ]
 }
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,11 +1,59 @@
-<h2 class="routetitle">Home!</h2>
+<div class="card-container">
+      <Card style="width: 360px;">
+        <PrimaryAction on:click={() => clicked++}>
+          <Media class="card-media-16x9" aspectRatio="16x9"></Media>
+          <Content class="mdc-typography--body2">
+            <h2 class="mdc-typography--headline6" style="margin: 0;">A card with media.</h2>
+            <h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">And a subtitle.</h3>
+            It's all in this card. It's a veritable smorgasbord of card features.
+          </Content>
+        </PrimaryAction>
+        <Actions>
+          <ActionButtons>
+            <Button on:click={() => clicked++}>
+              <Label>Action</Label>
+            </Button>
+            <Button on:click={() => clicked++}>
+              <Label>Another</Label>
+            </Button>
+          </ActionButtons>
+          <ActionIcons>
+            <IconButton on:click={() => clicked++} toggle aria-label="Add to favorites" title="Add to favorites">
+              <Icon class="material-icons" on>favorite</Icon>
+              <Icon class="material-icons">favorite_border</Icon>
+            </IconButton>
+            <IconButton class="material-icons" on:click={() => clicked++} title="Share">share</IconButton>
+            <IconButton class="material-icons" on:click={() => clicked++} title="More options">more_vert</IconButton>
+          </ActionIcons>
+        </Actions>
+      </Card>
+    </div>
 
-<p>Welcome to this sample code!</p>
-<p>Things to try:</p>
-<ul>
-    <li>Navigate around with the links and buttons (notice how certain links become active depending on the path</li>
-    <li>Try pressing the browsers' back and forward buttons</li>
-    <li>The "Replace current page" button will change the page without adding a new item in the history stack (try pressing the back button!)</li>
-    <li>Manually change the URL's fragment/hash</li>
-    <li>Try refreshing the page</li>
-</ul>
+    <div>
+    <TabBar tabs={['Home', 'Merchandise', 'About Us']} let:tab bind:active>
+      <!-- Notice that the `tab` property is required! -->
+      <Tab {tab}>
+        <Label>{tab}</Label>
+      </Tab>
+    </TabBar>
+
+    <div style="margin-top: 15px;">
+      Programmatically select:
+      {#each ['Home', 'Merchandise', 'About Us'] as tab}
+        <Button on:click={() => active = tab}><Label>{tab}</Label></Button>
+      {/each}
+    </div>
+
+    <pre class="status">Selected: {active}</pre>
+  </div>
+
+<script>
+  import Card, {Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons} from '@smui/card';
+  import Button, {Label} from '@smui/button';
+  import IconButton, {Icon} from '@smui/icon-button';
+  import List, {Item, Text} from '@smui/list';
+  import Tab from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+  let active = 'Home';
+  let clicked = 0;
+</script>
